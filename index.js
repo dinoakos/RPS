5a09d38db95257c2756a4f0cd0eca3ed8e7efbb6
dinoakos <akosdino87@gmail.com>
2023-11-24 16:06:44 +0100

    trying

--- index.js
+++ index.js
@@ -5,15 +5,19 @@ function choosen(kep){
     switch(kep){
         case 'rock':
             window.alert('rock');
+            document.getElementById('rock').style="background-color: teal";
             repeatedGreetings();
             break;
         case 'paper':
             window.alert('paper');
+            document.getElementById('paper').style="background-color: teal";
             repeatedGreetings();
             break;
         case 'scissors':
             window.alert('scissors');
+            document.getElementById('scissors').style="background-color: teal";
             repeatedGreetings();
+            
             break;
     }
 }
@@ -22,6 +26,7 @@ function choosen(kep){
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
 const repeatedGreetings = async () => {
+    disableIcons();
     for(let i=0; i<10;i++){
         await sleep(200)
         document.getElementById('rock2').style="transform: translateY(4vh)";
@@ -44,17 +49,24 @@ const repeatedGreetings = async () => {
   
   let bot=Math.floor(Math.random()*3)+1;
   
+  await sleep(210)
   switch(bot){
     case 1:
         document.getElementById('rock2').style="transform: translateY(-6vh)";
+        document.getElementById('paper2').style="transform: translateY(0vh)";
+        document.getElementById('scissors2').style="transform: translateY(0vh)";
         document.getElementById('rock2').style="background-color: teal";
         break;
     case 2:
         document.getElementById('paper2').style="transform: translateY(-6vh)";
+        document.getElementById('rock2').style="transform: translateY(0vh)";
+        document.getElementById('scissors2').style="transform: translateY(0vh)";
         document.getElementById('paper2').style="background-color: teal";
         break;
     case 3:
         document.getElementById('scissors2').style="transform: translateY(-6vh)";
+        document.getElementById('paper2').style="transform: translateY(0vh)";
+        document.getElementById('rock2').style="transform: translateY(0vh)";
         document.getElementById('scissors2').style="background-color: teal";
         break;
 }
@@ -62,18 +74,32 @@ const repeatedGreetings = async () => {
 await sleep(1000)
     if( (ckep=='rock' && bot==2 ) || (ckep=='paper' && bot==3) || (ckep=='scissors' && bot==1)){
         window.alert("Te vesztettél");
+        
     }
 
     if( (ckep=='rock' && bot==3 ) || (ckep=='paper' && bot==1) || (ckep=='scissors' && bot==2)){
         window.alert("Te Nyertél");
+        
     }
     if( (ckep=='rock' && bot==1 ) || (ckep=='paper' && bot==2) || (ckep=='scissors' && bot==3)){
         window.alert("Döntetlen");
+        
     }
+    enableIcons();
 
 
 }
 
+function disableIcons(){
+    document.getElementById('rock').onclick="#";
+    document.getElementById('paper').onclick="#";
+    document.getElementById('scissors').onclick="#";
+}
+function enableIcons(){
+    document.getElementById('rock').setAttribute("onclick","choosen('rock')");
+    document.getElementById('paper').setAttribute("onclick","choosen('paper')");
+    document.getElementById('scissors').setAttribute("onclick","choosen('scissors')");
+}
 
   
   
